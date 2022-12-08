3dd7fef93a6e90161a655bd6468eb8650fcbe33f
taran.daniil03@mail.ru <taran.daniil03@mail.ru>
2022-12-08 22:19:00 +0700

    В процессе

--- Лаб 8.3/MainForm.h
+++ Лаб 8.3/MainForm.h
@@ -12,21 +12,38 @@ namespace Лаб83 {
 	/// <summary>
 	/// Сводка для MainForm
 	/// </summary>
+	void Enemy_taking_turn() {
+
+	}
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
 	public:
 		MainForm(void)
 		{
+			pictarr = gcnew array <Bitmap^>(11);
+			pictarr[0] = gcnew Bitmap("Images\\1.png");
+			pictarr[1] = gcnew Bitmap("Images\\2.png");
+			pictarr[2] = gcnew Bitmap("Images\\3.png");
+			pictarr[3] = gcnew Bitmap("Images\\4.png");
+			pictarr[4] = gcnew Bitmap("Images\\5.png");
+			pictarr[5] = gcnew Bitmap("Images\\6.png");
+			pictarr[6] = gcnew Bitmap("Images\\7.png");
+			pictarr[7] = gcnew Bitmap("Images\\8.png");
+			pictarr[8] = gcnew Bitmap("Images\\9.png");
+			pictarr[9] = gcnew Bitmap("Images\\10.png");
+			pictarr[10] = gcnew Bitmap("Images\\0.png");
+			this->deck = gcnew Deck();
 			num_your_free_card_space_you = 1;
 			num_your_free_card_space_enemy = 1;
 			score_enemy = 0;
 			score_you = 0;
+			pass_you = 0;
+			pass_enemy = 0;
 			InitializeComponent();
 			//
 			//TODO: добавьте код конструктора
 			//
 		}
-
 	protected:
 		/// <summary>
 		/// Освободить все используемые ресурсы.
@@ -39,8 +56,8 @@ namespace Лаб83 {
 			}
 		}
 	private: System::Windows::Forms::Button^ button_take_card;
-	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label_pass_enemy;
+	private: System::Windows::Forms::Label^ label_pass_you;
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::DataGridView^ dataGridView1;
 	private: System::Windows::Forms::Label^ label4;
@@ -61,11 +78,63 @@ namespace Лаб83 {
 	private: System::Windows::Forms::PictureBox^ pictureBox_enemy_card_5;
 	private: System::Windows::Forms::PictureBox^ pictureBox_enemy_card_4;
 	private: System::Windows::Forms::PictureBox^ pictureBox_enemy_card_3;
+	private: System::Windows::Forms::Button^ button_puss_you;
 	private: System::Windows::Forms::PictureBox^ pictureBox_enemy_card_2;
 	private: System::Windows::Forms::PictureBox^ pictureBox_enemy_card_1;
 	private: int score_enemy;
 	private: int score_you;
-	private:
+	private: int pass_you;
+	private: int pass_enemy;
+	private: void Enemy_Taking_turn() {
+		if (pass_you = 1 && score_enemy <= score_you) {
+			if (score_enemy <= 17) {
+				Card^ current_card;
+				current_card = this->deck->GetUpperCard();
+				if (num_your_free_card_space_enemy == 1) {
+					pictureBox_enemy_card_1->Image = current_card->picture;
+					score_enemy += current_card->Get_Cost();
+					label_score_enemy->Text = score_enemy.ToString() + "\\21";
+					if (score_enemy > 21)
+						label_score_enemy->ForeColor = System::Drawing::Color::Red;
+				}
+				if (num_your_free_card_space_enemy == 2) {
+					pictureBox_enemy_card_2->Image = current_card->picture;
+					score_enemy += current_card->Get_Cost();
+					label_score_enemy->Text = score_enemy.ToString() + "\\21";
+					if (score_enemy > 21)
+						label_score_enemy->ForeColor = System::Drawing::Color::Red;
+				}
+				if (num_your_free_card_space_enemy == 3) {
+					pictureBox_enemy_card_3->Image = current_card->picture;
+					score_enemy += current_card->Get_Cost();
+					label_score_enemy->Text = score_enemy.ToString() + "\\21";
+					if (score_enemy > 21)
+						label_score_enemy->ForeColor = System::Drawing::Color::Red;
+				}
+				if (num_your_free_card_space_enemy == 4) {
+					pictureBox_enemy_card_4->Image = current_card->picture;
+					score_enemy += current_card->Get_Cost();
+					label_score_enemy->Text = score_enemy.ToString() + "\\21";
+					if (score_enemy > 21)
+						label_score_enemy->ForeColor = System::Drawing::Color::Red;
+				}
+				if (num_your_free_card_space_enemy == 5) {
+					pictureBox_enemy_card_5->Image = current_card->picture;
+					score_enemy += current_card->Get_Cost();
+					label_score_enemy->Text = score_enemy.ToString() + "\\21";
+					if (score_enemy > 21)
+						label_score_enemy->ForeColor = System::Drawing::Color::Red;
+				}
+				num_your_free_card_space_enemy++;
+				deck->Reshafle();
+			}
+			else
+				pass_enemy = 1;
+		}
+		else
+			pass_enemy = 1;
+	}
+	private: array <Bitmap^>^ pictarr = gcnew array <Bitmap^>(10);
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
@@ -78,8 +147,8 @@ namespace Лаб83 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
 			System::Windows::Forms::PictureBox^ pictureBox11;
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MainForm::typeid));
 			this->pictureBox_enemy_card_5 = (gcnew System::Windows::Forms::PictureBox());
 			this->pictureBox_enemy_card_4 = (gcnew System::Windows::Forms::PictureBox());
 			this->pictureBox_enemy_card_3 = (gcnew System::Windows::Forms::PictureBox());
@@ -91,8 +160,8 @@ namespace Лаб83 {
 			this->pictureBox_your_card_4 = (gcnew System::Windows::Forms::PictureBox());
 			this->pictureBox_your_card_1 = (gcnew System::Windows::Forms::PictureBox());
 			this->button_take_card = (gcnew System::Windows::Forms::Button());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label_pass_enemy = (gcnew System::Windows::Forms::Label());
+			this->label_pass_you = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
 			this->You = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
@@ -101,13 +170,14 @@ namespace Лаб83 {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->label_score_you = (gcnew System::Windows::Forms::Label());
 			this->label_score_enemy = (gcnew System::Windows::Forms::Label());
+			this->button_puss_you = (gcnew System::Windows::Forms::Button());
 			pictureBox11 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(pictureBox11))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_5))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_4))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_3))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_1))->BeginInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(pictureBox11))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_5))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_2))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_3))->BeginInit();
@@ -115,6 +185,17 @@ namespace Лаб83 {
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_1))->BeginInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// pictureBox11
+			// 
+			pictureBox11->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox11.Image")));
+			pictureBox11->InitialImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox11.InitialImage")));
+			pictureBox11->Location = System::Drawing::Point(925, 304);
+			pictureBox11->Name = L"pictureBox11";
+			pictureBox11->Size = System::Drawing::Size(63, 99);
+			pictureBox11->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			pictureBox11->TabIndex = 19;
+			pictureBox11->TabStop = false;
 			// 
 			// pictureBox_enemy_card_5
 			// 
@@ -175,17 +256,6 @@ namespace Лаб83 {
 			this->pictureBox_enemy_card_1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
 			this->pictureBox_enemy_card_1->TabIndex = 14;
 			this->pictureBox_enemy_card_1->TabStop = false;
-			// 
-			// pictureBox11
-			// 
-			pictureBox11->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox11.Image")));
-			pictureBox11->InitialImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox11.InitialImage")));
-			pictureBox11->Location = System::Drawing::Point(925, 304);
-			pictureBox11->Name = L"pictureBox11";
-			pictureBox11->Size = System::Drawing::Size(63, 99);
-			pictureBox11->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
-			pictureBox11->TabIndex = 19;
-			pictureBox11->TabStop = false;
 			// 
 			// pictureBox_your_card_5
 			// 
@@ -249,7 +319,7 @@ namespace Лаб83 {
 			// 
 			// button_take_card
 			// 
-			this->button_take_card->Location = System::Drawing::Point(884, 480);
+			this->button_take_card->Location = System::Drawing::Point(895, 511);
 			this->button_take_card->Name = L"button_take_card";
 			this->button_take_card->Size = System::Drawing::Size(104, 44);
 			this->button_take_card->TabIndex = 5;
@@ -257,29 +327,29 @@ namespace Лаб83 {
 			this->button_take_card->UseVisualStyleBackColor = true;
 			this->button_take_card->Click += gcnew System::EventHandler(this, &MainForm::button_take_card_Click);
 			// 
-			// label1
+			// label_pass_enemy
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label_pass_enemy->AutoSize = true;
+			this->label_pass_enemy->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label1->Location = System::Drawing::Point(662, 136);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(61, 29);
-			this->label1->TabIndex = 15;
-			this->label1->Text = L"Пас!";
-			this->label1->Visible = false;
+			this->label_pass_enemy->Location = System::Drawing::Point(662, 136);
+			this->label_pass_enemy->Name = L"label_pass_enemy";
+			this->label_pass_enemy->Size = System::Drawing::Size(61, 29);
+			this->label_pass_enemy->TabIndex = 15;
+			this->label_pass_enemy->Text = L"Пас!";
+			this->label_pass_enemy->Visible = false;
 			// 
-			// label2
+			// label_pass_you
 			// 
-			this->label2->AutoSize = true;
-			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->label_pass_you->AutoSize = true;
+			this->label_pass_you->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(662, 444);
-			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(61, 29);
-			this->label2->TabIndex = 16;
-			this->label2->Text = L"Пас!";
-			this->label2->Visible = false;
+			this->label_pass_you->Location = System::Drawing::Point(662, 444);
+			this->label_pass_you->Name = L"label_pass_you";
+			this->label_pass_you->Size = System::Drawing::Size(61, 29);
+			this->label_pass_you->TabIndex = 16;
+			this->label_pass_you->Text = L"Пас!";
+			this->label_pass_you->Visible = false;
 			// 
 			// label3
 			// 
@@ -359,11 +429,22 @@ namespace Лаб83 {
 			this->label_score_enemy->TabIndex = 23;
 			this->label_score_enemy->Text = L"0/21";
 			// 
+			// button_puss_you
+			// 
+			this->button_puss_you->Location = System::Drawing::Point(895, 464);
+			this->button_puss_you->Name = L"button_puss_you";
+			this->button_puss_you->Size = System::Drawing::Size(104, 41);
+			this->button_puss_you->TabIndex = 24;
+			this->button_puss_you->Text = L"Спасовать";
+			this->button_puss_you->UseVisualStyleBackColor = true;
+			this->button_puss_you->Click += gcnew System::EventHandler(this, &MainForm::button_puss_you_Click);
+			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1046, 600);
+			this->Controls->Add(this->button_puss_you);
 			this->Controls->Add(this->label_score_enemy);
 			this->Controls->Add(this->label_score_you);
 			this->Controls->Add(this->button2);
@@ -371,8 +452,8 @@ namespace Лаб83 {
 			this->Controls->Add(pictureBox11);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->label3);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->label1);
+			this->Controls->Add(this->label_pass_you);
+			this->Controls->Add(this->label_pass_enemy);
 			this->Controls->Add(this->pictureBox_enemy_card_1);
 			this->Controls->Add(this->pictureBox_enemy_card_2);
 			this->Controls->Add(this->pictureBox_enemy_card_3);
@@ -386,12 +467,12 @@ namespace Лаб83 {
 			this->Controls->Add(this->pictureBox_your_card_5);
 			this->Name = L"MainForm";
 			this->Text = L"MainForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(pictureBox11))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_5))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_4))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_3))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_enemy_card_1))->EndInit();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(pictureBox11))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_5))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_2))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox_your_card_3))->EndInit();
@@ -404,23 +485,36 @@ namespace Лаб83 {
 		}
 #pragma endregion
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
-	array <Bitmap^>^ pictarr = gcnew array <Bitmap^>(10);
-	pictarr[0] = gcnew Bitmap("Images\\1.png");
-	pictarr[1] = gcnew Bitmap("Images\\2.png");
-	pictarr[2] = gcnew Bitmap("Images\\3.png");
-	pictarr[3] = gcnew Bitmap("Images\\4.png");
-	pictarr[4] = gcnew Bitmap("Images\\5.png");
-	pictarr[5] = gcnew Bitmap("Images\\6.png");
-	pictarr[6] = gcnew Bitmap("Images\\7.png");
-	pictarr[7] = gcnew Bitmap("Images\\8.png");
-	pictarr[8] = gcnew Bitmap("Images\\9.png");
-	pictarr[9] = gcnew Bitmap("Images\\10.png");
+	delete this->deck;
 	this->deck = gcnew Deck();
 	for (size_t i = 0; i < 10; i++)
 	{
 		this->deck->AddCard(i+1,i, pictarr[i]);
 	}
 	this->deck->Reshafle();
+
+	pictureBox_enemy_card_1->Image = pictarr[10];
+	pictureBox_enemy_card_2->Image = pictarr[10];
+	pictureBox_enemy_card_3->Image = pictarr[10];
+	pictureBox_enemy_card_4->Image = pictarr[10];
+	pictureBox_enemy_card_5->Image = pictarr[10];
+	pictureBox_your_card_1->Image = pictarr[10];
+	pictureBox_your_card_2->Image = pictarr[10];
+	pictureBox_your_card_3->Image = pictarr[10];
+	pictureBox_your_card_4->Image = pictarr[10];
+	pictureBox_your_card_5->Image = pictarr[10];
+	label_pass_enemy->Hide();
+	label_pass_you->Hide();
+	label_score_enemy->Text = "0/21";
+	label_score_you->Text = "0/21";
+
+	num_your_free_card_space_you = 1;
+	num_your_free_card_space_enemy = 1;
+	score_enemy = 0;
+	score_you = 0;
+	pass_you = 0;
+	pass_enemy = 0;
+
 }
 private: System::Void button_take_card_Click(System::Object^ sender, System::EventArgs^ e) {
 	if (num_your_free_card_space_you < 6) {
@@ -465,49 +559,19 @@ private: System::Void button_take_card_Click(System::Object^ sender, System::Eve
 	deck->Reshafle();
 
 	// Enemy turn started
-	if (score_enemy <= 17) {
-		Card^ current_card;
-		current_card = this->deck->GetUpperCard();
-		if (num_your_free_card_space_enemy == 1) {
-			pictureBox_enemy_card_1->Image = current_card->picture;
-			score_enemy += current_card->Get_Cost();
-			label_score_enemy->Text = score_enemy.ToString() + "\\21";
-			if (score_enemy > 21)
-				label_score_enemy->ForeColor = System::Drawing::Color::Red;
-		}
-		if (num_your_free_card_space_enemy == 2) {
-			pictureBox_enemy_card_2->Image = current_card->picture;
-			score_enemy += current_card->Get_Cost();
-			label_score_enemy->Text = score_enemy.ToString() + "\\21";
-			if (score_enemy > 21)
-				label_score_enemy->ForeColor = System::Drawing::Color::Red;
-		}
-		if (num_your_free_card_space_enemy == 3) {
-			pictureBox_enemy_card_3->Image = current_card->picture;
-			score_enemy += current_card->Get_Cost();
-			label_score_enemy->Text = score_enemy.ToString() + "\\21";
-			if (score_enemy > 21)
-				label_score_enemy->ForeColor = System::Drawing::Color::Red;
-		}
-		if (num_your_free_card_space_enemy == 4) {
-			pictureBox_enemy_card_4->Image = current_card->picture;
-			score_enemy += current_card->Get_Cost();
-			label_score_enemy->Text = score_enemy.ToString() + "\\21";
-			if (score_enemy > 21)
-				label_score_enemy->ForeColor = System::Drawing::Color::Red;
-		}
-		if (num_your_free_card_space_enemy == 5) {
-			pictureBox_enemy_card_5->Image = current_card->picture;
-			score_enemy += current_card->Get_Cost();
-			label_score_enemy->Text = score_enemy.ToString() + "\\21";
-			if (score_enemy > 21)
-				label_score_enemy->ForeColor = System::Drawing::Color::Red;
-		}
-		num_your_free_card_space_enemy++;
-		deck->Reshafle();
-	}
+	if(pass_enemy=0)
+	Enemy_Taking_turn();
 
 	}
+}
+private: System::Void button_puss_you_Click(System::Object^ sender, System::EventArgs^ e) {
+	pass_you = 1;
+	label_pass_you->Visible = 1;
+	while (pass_enemy != 1 && num_your_free_card_space_enemy != 6) {
+		Enemy_Taking_turn();
+	}
+	label_pass_enemy->Visible = 1;
+	
 }
 };
 }
